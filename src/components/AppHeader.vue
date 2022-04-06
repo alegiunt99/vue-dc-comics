bd9bd82e7914456bdbfe9b7d5d854730fa0de395
Alessandro Giunta <alegiunt99@gmail.com>
2022-04-06 16:11:47 +0200

    Inserisco il contenuto

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,29 @@
 <template>
-  <header>
+  <header class="header-page">
+    <!-- creo la classe container che centra il contenuto-->
+    <div class="container">
 
+      <!-- il div del logo -->
+      <div class="logo">
+        <img src="../assets/dc-logo.png" alt="Logo DC-Comics">
+      </div>
+
+      <!-- il div della barra di navigazione -->
+      <div class="nav-bar">
+        <div class="nav-link"><a href="#"></a>CHARACTERS</div>
+        <div class="nav-link"><a href="#"></a>CHARACTERS</div>
+        <div class="nav-link"><a href="#"></a>CHARACTERS</div>
+        <div class="nav-link"><a href="#"></a>CHARACTERS</div>
+        <div class="nav-link"><a href="#"></a>CHARACTERS</div>
+        <div class="nav-link"><a href="#"></a>CHARACTERS</div>
+        <div class="nav-link"><a href="#"></a>CHARACTERS</div>
+        <div class="nav-link"><a href="#"></a>CHARACTERS</div>
+        <div class="nav-link"><a href="#"></a>CHARACTERS</div>
+        <div class="nav-link"><a href="#"></a>CHARACTERS</div>
+      </div>
+
+    </div>
+    
   </header>
 </template>
 
